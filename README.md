90acb0aefc49e98b50dcb82c3a2e24533c6ea5b7
utsav <utsavagarwal2014@gmail.com>
2022-03-13 23:57:13 +0000

    adding support to generate images

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Requires opencv, pillow, numpy
 Implements image to asci from scratch.
 
 press 'q' when dialog box appears to capture image
+add argument 1/0 for inverted(black-on-white)/normal(white-on-black) image
+  - eg: python3 asci_face.py 1 //gives inverted image
--- asci_face.py
+++ asci_face.py
@@ -1,30 +1,52 @@
 import cv2
 import numpy as np
-from PIL import Image
 import os
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import sys
+
 
 
 # img_path = 'test1.jpeg'
 img_path_temp = 'test_temp.jpeg'
 img_op = "whoami.txt"
-sens = 90
-inverted = 0
+img_bg_path = "whoami.png"
+
+sens = 120
+inverted = 1
+img_size = (250,100)
+txt_size = (140,40)
+
+inverted = int(sys.argv[-1])
+
+text_color = 0
+bg_color = 0
+
+if(inverted):
+    bg_color = 0
+    text_color = 255
+else:
+    bg_color = 255
+    text_color = 0
 
 def get_image():
 
     cam = cv2.VideoCapture(0)
 
     while True:
-        status, img = cam.read()
+        status, img = cam.read(img_size)
         cv2.imshow("Webcam", img)
         if cv2.waitKey(1) == ord('q'):
             break
         cv2.imwrite(img_path_temp, img)
 
-def resize_image(img_path):
+def resize_image(img_path,size):
 
     img = Image.open(img_path)
-    img_resized = img.resize((170,54))
+    img_resized = img.resize(size)
     img_resized.save(img_path_temp)
 
 def make_new(img_path):
@@ -44,35 +66,80 @@ def make_new(img_path):
     return pic_array
 
 
+def make_blank_slate(img_size):
+    bg = np.ones((16*img_size[1],9*img_size[0]))
+    bg[bg == 1] = bg_color
+    cv2.imwrite(img_bg_path,bg)
+
+def write_to_slate(text):
+    img = Image.open(img_bg_path)
+    img_draw = ImageDraw.Draw(img)
+
+    img_font = ImageFont.truetype('FreeMono.ttf', 1)
+
+
+    for i in range(len(text)):
+        for j in range(len(text[i])):
+            if(
+            i>1 and j>1 and i<len(text)-1 and j<len(text[i])-1 and\
+            text[i-1][j] and text[i+1][j] and\
+            text[i][j-1] and text[i][j+1] and\
+            text[i-1][j-1] and text[i+1][j+1] and\
+            text[i+1][j-1] and text[i-1][j+1]and\
+            text[i][j]):
+                img_draw.text((9*j, 16*i),"@",fill=int(text_color))
+            elif(text[i][j]):
+                img_draw.text((9*j, 16*i),"#",fill=int(text_color))
+
+
+    img.show()
+    img.save(img_bg_path)
+
+
+def write_to_text(text):
+
+    global img_op
+
+    f = open(img_op,"w")
+    f.write("")
+    f.close()
+
+    #saving to text
+    f = open(img_op,"a")
+    for i in range(len(text)):
+        for j in range(len(text[i])):
+            if(
+            i>1 and j>1 and i<len(text)-1 and j<len(text[i])-1 and\
+            text[i-1][j] and text[i+1][j] and\
+            text[i][j-1] and text[i][j+1] and\
+            text[i-1][j-1] and text[i+1][j+1] and\
+            text[i+1][j-1] and text[i-1][j+1]and\
+            text[i][j]):
+                f.write(str('@'))
+            elif(text[i][j]):
+                f.write(str('#'))
+            else:
+                f.write(str(' '))
+        f.write("\n")
+    f.close()
+
+
 
 get_image()
-resize_image(img_path_temp)
+
+#prep for image
+resize_image(img_path_temp,img_size)
 text = make_new(img_path_temp)
 
+#make image
+make_blank_slate(img_size)
+write_to_slate(text)
 
+#prep for text
+resize_image(img_path_temp,txt_size)
+text = make_new(img_path_temp)
+
+#remove temp files
 os.remove(img_path_temp)
 
-f = open(img_op,"w")
-f.write("")
-f.close()
-
-f = open(img_op,"a")
-for i in range(len(text)):
-    for j in range(len(text[i])):
-
-
-        if(
-        i>1 and j>1 and i<len(text)-1 and j<len(text[i])-1 and\
-        text[i-1][j] and text[i+1][j] and\
-        text[i][j-1] and text[i][j+1] and\
-        text[i-1][j-1] and text[i+1][j+1] and\
-        text[i+1][j-1] and text[i-1][j+1]and\
-        text[i][j]):
-            f.write(str('@'))
-
-        elif(text[i][j]):
-            f.write(str('#'))
-        else:
-            f.write(str(' '))
-    f.write("\n")
-f.close()
+write_to_text(text)
